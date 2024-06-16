de87b31f2b6b8a48b92bbf54afd73e07f1d9a48a
brodriguez <bastian3967a@gmail.com>
2024-06-16 00:26:29 -0400

    create api for products

--- backend_bcs/api/models.py
+++ backend_bcs/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    size = models.TextField()
+    gender = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} | {self.size} | {self.gender}'
--- backend_bcs/api/views.py
+++ backend_bcs/api/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+# api/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        size = request.query_params.get('size')
+        gender = request.query_params.get('gender')
+        price_range = request.query_params.get('priceRange')
+
+        if size:
+            products = products.filter(size=size)
+        if gender:
+            products = products.filter(gender=gender)
+        if price_range:
+            min_price, max_price = map(int, price_range.split('-'))
+            products = products.filter(price__gte=min_price, price__lte=max_price)
+
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        if not request.user.is_staff:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    def get(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        if not request.user.is_staff:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        if not request.user.is_staff:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
